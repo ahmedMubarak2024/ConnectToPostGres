e9d3ccfbdf57b76673aa81754035a7f0d600c9ac
ahmed <Ahmed Mubarak>
2022-12-09 22:41:14 +0200

    test connection all working

---
+++ migrations/sqls/20221209163939-mythical-worlds-table-up.sql
@@ -0,0 +1,6 @@
+/* Replace with your SQL commands */
+CREATE TABLE mytical_wepon (
+id SERIAL PRIMARY KEY,
+name VARCHAR(50),
+count integer
+);
---
+++ src/models/MythicalWeopon.ts
@@ -0,0 +1,38 @@
+import { client } from "../database";
+
+
+export type Weapon = {
+    id:number,
+    name:string,
+    count:number
+}
+
+export class MythicalWeaponStore{
+    async index(): Promise<Weapon[]> {
+        try{
+            const connection = await client.connect()
+            const result = await connection.query('SELECT * FROM mytical_wepon')
+            connection.release()
+            return result.rows
+        }catch(err){
+            throw new Error("Could not connect to database "+err)
+        }
+    }
+
+    async insertNewWeapon(weapon:Weapon): Promise<Weapon>{
+        try{
+            const connection = await client.connect()
+            const result = await connection.query('INSERT INTO mytical_wepon (name,count) VALUES ('+weapon.name+','+weapon.count+')')
+            connection.release()
+            return result.rows[0]
+        }catch(err){
+            throw new Error("Could not connect to database "+err)
+        }
+
+    }
+}
+
+new MythicalWeaponStore().index().then((result:Weapon[]) => {
+    console.log(result);
+    
+})
